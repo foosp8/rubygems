81d4ae4fda5ba3ae2582210e4ef88e4abf51b4a0
foosp8 <foosp8@hotmail.com>
2020-12-12 16:20:18 +0800

    install Font Awesome via CLI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+import "@fortawesome/fontawesome-free/css/all"
 require("stylesheets/application.scss")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
