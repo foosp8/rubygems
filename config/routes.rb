2fa19a3e2a746f61bdef47b6af5c41f10203f590
foosp8 <foosp8@hotmail.com>
2021-01-28 12:44:30 +0800

    updated lesson nested under Course

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :lessons
   devise_for :users
-  resources :courses
-    resources :users, only: [:index, :edit, :show, :update]
-    get 'home/index'
-    get 'home/activity'
+  resources :courses do
+    resources :lessons
+  end
+
+  resources :users, only: [:index, :edit, :show, :update]
+
+  get 'home/index'
+  get 'home/activity'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
