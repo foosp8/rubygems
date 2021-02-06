0848693b2e18426291b00bae2e6219f40f12a860
foosp8 <foosp8@hotmail.com>
2021-02-07 02:30:05 +0800

    create Button for user to Review a Course

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -17,4 +17,18 @@ module CoursesHelper
     end
   end
 
+
+  def review_button(course)
+     user_course = course.enrollments.where(user: current_user)
+     if current_user
+       if user_course.any?
+         if user_course.pending_review.any?
+           link_to 'Add a review', edit_enrollment_path(user_course.first)
+         else
+           link_to 'Thanks for reviewing! Your Review', enrollment_path(user_course.first)
+         end
+       end
+     end
+   end
+
 end
